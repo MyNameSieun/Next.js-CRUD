1e5e09fc7d6c22604438fab58e66e48c0a8d9f06
sieun <charm516@naver.com>
2025-01-21 01:22:32 +0900

    [feat] - Shadcn Ui 라이브러리를 활용한 퍼블리싱 2

---
+++ app/create/page.tsx
@@ -0,0 +1,61 @@
+import { Progress } from "@/components/ui/progress";
+import styles from "./page.module.scss";
+import { Button } from "@/components/ui/button";
+import LabelCalendar from "@/components/common/calendar/LabelCalendar";
+import Image from "next/image";
+
+const CreatePage = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.container__header}>
+        <div className={styles.container__header__contents}>
+          <input
+            type="text"
+            placeholder="Enter title Here"
+            className={styles.input}
+          />
+          <div className={styles.progressBar}>
+            <span className={styles.progressBar__status}>0/10 completed</span>
+            {/* 프로그래스 바 UI */}
+            <Progress
+              value={33}
+              className="w-[30%]"
+              indicatorColor="bg-green-500"
+            />
+          </div>
+          <div className={styles.calendarBox}>
+            <div className={styles.calendarBox__calendar}>
+              {/* 캘린더 UI */}
+              <LabelCalendar label="From" readonly={true} />
+              <LabelCalendar label="To" readonly={true} />
+            </div>
+            <Button
+              variant={"outline"}
+              className="w-[25%] border-orange-500 bg-orange-400 text-white hover:bg-orange-400 hover:text-white"
+            >
+              Add New Board
+            </Button>
+          </div>
+        </div>
+      </header>
+      <main className={styles.container__body}>
+        <div className={styles.container__body__infoBox}>
+          <span className={styles.title}>There is no borad Yet.</span>
+          <span className={styles.subTitle}>
+            Click the button and start flashing!
+          </span>
+          <button className={styles.button}>
+            <Image
+              src="/assets/images/round-button.svg"
+              alt="round-button"
+              width={100}
+              height={100}
+            />
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default CreatePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 // app/page.tsx
 import { Button } from "@/components/ui/button";
 import styles from "./page.module.scss";
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <div className={styles.container__onBoarding}>
@@ -15,6 +18,7 @@ const HomePage = () => {
         </div>
         {/* 페이지 추가버튼 */}
         <Button
+          onClick={() => router.push("/create")}
           variant="outline"
           className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500"
         >
